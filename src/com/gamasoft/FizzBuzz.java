7e918dbf3a2618ac5a598c8e542dec4e3b605e05
Uberto Barbini <uberto@ubiland.net>
2014-09-30 22:43:52 +0100

    first commit

---
+++ src/com/gamasoft/FizzBuzz.java
@@ -0,0 +1,16 @@
+package com.gamasoft;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: uberto
+ * Date: 30/09/14
+ * Time: 22:33
+ * To change this template use File | Settings | File Templates.
+ */
+public class FizzBuzz {
+    public static void Emit(int from, int to) {
+        for (int i = from; i < to; i++) {
+            System.out.println(i);
+        }
+    }
+}
---
+++ test/com/gamasoft/FizzBuzzTest.java
@@ -0,0 +1,21 @@
+package com.gamasoft;
+
+import org.junit.Test;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: uberto
+ * Date: 30/09/14
+ * Time: 22:34
+ * To change this template use File | Settings | File Templates.
+ */
+public class FizzBuzzTest {
+
+
+    @Test
+    public void testEmit() throws Exception {
+
+        FizzBuzz.Emit(1, 100);
+
+    }
+}
